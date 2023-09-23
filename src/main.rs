3e8b06408517bedb2ce8395384ab2948c1363109
Vladimir Kidyaev <krios2146@gmail.com>
2023-09-23 15:08:39 +0700

    feat: add user input handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Number guessing game begins!");
+
+    println!("Enter the guess:");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("Your guess is: {guess}")
 }
