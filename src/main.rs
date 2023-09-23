7d47abaab114f9d08be0fb4926381bad6b15b4ee
Vladimir Kidyaev <krios2146@gmail.com>
2023-09-23 16:22:06 +0700

    feat: add loop and error handling

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,40 @@
 use std::cmp::Ordering;
 use std::io;
+
 use rand::Rng;
 
 fn main() {
     println!("Number guessing game begins!");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let secret_number: i32 = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret number is: {secret_number}");
+    loop {
+        println!("Enter the guess:");
 
-    println!("Enter the guess:");
+        let mut guess = String::new();
 
-    let mut guess = String::new();
+        if io::stdin().read_line(&mut guess).is_err() {
+            println!("Failed to read entered guess");
+            continue;
+        }
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        let guess: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter the number");
+                continue;
+            }
+        };
 
-    let guess: i32 = guess.trim().parse()
-        .expect("Please type a number");
+        println!("Your guess is: {guess}");
 
-    println!("Your guess is: {guess}");
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small"),
-        Ordering::Equal => println!("You win"),
-        Ordering::Greater => println!("Too big")
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You win");
+                break;
+            }
+        }
     }
 }
