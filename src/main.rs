b4d6c3c7927493b99e03d6248bd6beed0d929416
Vladimir Kidyaev <krios2146@gmail.com>
2023-09-23 15:19:00 +0700

    feat: add random number generation

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Number guessing game begins!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
 
     println!("Enter the guess:");
 
